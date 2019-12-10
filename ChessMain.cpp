7aa687717881baf951830e3b645e77b88e06283f
Wenjun Li <wl419@gpu02.doc.ic.ac.uk>
2019-12-10 23:30:09 +0000

    final

--- ChessMain.cpp
+++ ChessMain.cpp
@@ -4,116 +4,6 @@
 using namespace std;
 
 int main() {
-
-  cout << "=========================\n";
-  cout << "      Test castling\n";
-  cout << "=========================\n\n";
-
-  ChessBoard cb;
-  cout << '\n';
-  
-  cb.resetBoard();
-  cout << '\n';
-
-  cb.submitMove("C2", "C4");
-  cb.submitMove("F7", "F5");
-  cout << '\n';
-
-  cb.submitMove("B1", "C3");
-  cb.submitMove("G8", "F6");
-  cout << '\n';
-
-  cb.submitMove("B2", "B4");
-  cb.submitMove("G7", "G6");
-  cout << '\n';
-
-  cb.submitMove("C1", "A3");
-  cb.submitMove("F8", "H6");
-  cout << '\n';
-
-  cb.submitMove("D1", "A4");
-  cb.submitMove("G6", "G5");
-  cout << '\n';
-
-  cb.submitMove("E1", "C1"); // castling
-  cb.submitMove("E8", "G8"); // castling again
-  cout << '\n';
-
-  cb.submitMove("A4", "A5");
-  cb.submitMove("A7", "A6");
-  cout << '\n';
-
-    /*
-  
-  cout << "========================\n";
-  cout << "  Stalemate Testing 1\n";
-  cout << "========================\n\n"; 
-
-  ChessBoard cb;
-  cout << '\n';
-
-  cb.submitMove("E2", "E3");
-  cout << '\n';
-
-  cb.submitMove("A7", "A5");
-  cout << '\n';
-
-  cb.submitMove("D1", "H5");
-  cout << '\n';
-
-  cb.submitMove("A8", "A6");
-  cout << '\n';
-
-  cb.submitMove("H5", "A5");
-  cout << '\n';
-
-  cb.submitMove("H7", "H5");
-  cout << '\n';
-
-  cb.submitMove("H2", "H4");
-  cout << '\n';
-
-  cb.submitMove("A6", "H6");
-  cout << '\n';
-
-  cb.submitMove("A5", "C7");
-  cout << '\n';
-
-  cb.submitMove("F7", "F6");
-  cout << '\n';
-
-  cb.submitMove("C7", "D7");
-  cout << '\n';
-
-  cb.submitMove("E8", "F7");
-  cout << '\n';
-
-  cb.submitMove("D7", "B7");
-  cout << '\n';
-
-  cb.submitMove("D8", "D3");
-  cout << '\n';
-
-  cb.submitMove("B7", "B8");
-  cout << '\n';
-
-  cb.submitMove("D3", "H7");
-  cout << '\n';
-
-  cb.submitMove("B8", "C8");
-  cout << '\n';
-
-  cb.submitMove("F7", "G6");
-  cout << '\n';
-
-  cb.submitMove("C8", "E6");
-  cout << '\n';
-
-  cb.submitMove("G6","F5");
-  cout << '\n';
-    */
-
-  /*  
 	cout << "========================\n";
 	cout << "Testing the Chess Engine\n";
 	cout << "========================\n\n";
@@ -182,6 +72,6 @@ int main() {
 
 	cb.submitMove("D3", "G6");
 	cout << '\n';
-  */
+
 	return 0;
 }
